eeb42ea16449e2739058a82cf758cc1af0b0a694
snehaljha <snehaljha08@gmail.com>
2024-03-31 16:47:34 +0530

    adds Item to list

--- src/main/java/com/gnc/todo/model/TodoList.java
+++ src/main/java/com/gnc/todo/model/TodoList.java
@@ -1,9 +1,13 @@
 package com.gnc.todo.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import lombok.Data;
 
 @Data
 public class TodoList {
     private Long id;
     private String name;
+    private List<ListItem> items = new ArrayList<>(); 
 }
