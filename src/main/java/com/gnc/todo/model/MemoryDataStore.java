f5982b8952bb6e69cb0cdb92a5ef138c2c91a70e
snehaljha <snehaljha08@gmail.com>
2024-03-31 17:53:40 +0530

    adds get all Lists

--- src/main/java/com/gnc/todo/model/MemoryDataStore.java
+++ src/main/java/com/gnc/todo/model/MemoryDataStore.java
@@ -1,5 +1,7 @@
 package com.gnc.todo.model;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import java.util.TreeMap;
@@ -65,6 +67,10 @@ public class MemoryDataStore {
         }
 
         return Optional.empty();
+    }
+
+    public List<TodoList> getAllTodoLists() {
+        return new ArrayList<>(listMap.values());
     }
 
 }
